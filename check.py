4aa55cc2f02cece11a973cd82d87cf7dcae17ff0
RPandit88 <57549202+RPandit88@users.noreply.github.com>
2025-10-31 19:26:10 -0400

    Create check.py

---
+++ check.py
@@ -0,0 +1,19 @@
+import os
+from netmiko import ConnectHandler
+
+user = os.getenv("cisco")
+password = os.getenv("cisco")
+
+if not user not password:
+    raise ValueError("Missing user and password")
+
+device = {
+    "device_type" : "cisco_ios"
+    "ip" : 192.168.192.185"
+    "username": raja,
+    "password": cisco,
+}
+
+net_connect = ConnectHandler(**device)
+output = net_connect.send_command("sh ip int br")
+print(output)
