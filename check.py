26bbcf19766e51f9b1660b388ea54cced272c561
RPandit88 <57549202+RPandit88@users.noreply.github.com>
2025-10-31 22:50:37 -0400

    Update check.py

--- check.py
+++ check.py
@@ -4,12 +4,12 @@ from netmiko import ConnectHandler
 user = os.getenv("cisco")
 password = os.getenv("cisco")
 
-if not user not password:
-    raise ValueError("Missing user and password")
+#if not user not password:
+ #   raise ValueError("Missing user and password")
 
 device = {
     "device_type" : "cisco_ios"
-    "ip" : 192.168.192.185"
+    "ip" : "192.168.192.185"
     "username": raja,
     "password": cisco,
 }
